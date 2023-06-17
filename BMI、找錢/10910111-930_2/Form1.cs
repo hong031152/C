b7c781496c581e1164ae8a6408fc94d14d61b633
hong031152 <102577587+hong031152@users.noreply.github.com>
2023-06-17 18:07:07 +0800

    Add files via upload

---
+++ BMI、找錢/10910111-930_2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10910111_930_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int money = Convert.ToInt32(textBox1.Text);
+            int coin10 = 0, coin5 = 0, coin1 = 0;
+            coin10 = money / 10;
+            money = money % 10;
+            coin5 = money / 5;
+            coin1 = money % 5;
+            label2.Text = "10元"+coin10+"枚,5元"+coin5+"枚,1元"+coin1+"枚";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double h1 = Convert.ToDouble(textBox2.Text);
+            double w1 = Convert.ToDouble(textBox3.Text);
+            h1 = h1 / 100;          
+            label5.Text = "BMI="+w1 / (h1*h1); 
+
+        }
+    }
+}
