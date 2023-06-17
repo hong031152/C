a712cd628d9494e5646b75387c218f3b4dc6c915
hong031152 <102577587+hong031152@users.noreply.github.com>
2023-06-17 18:12:31 +0800

    Add files via upload

---
+++ 簡單登入系統/10910111_3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10910111_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] stuId = new string[] { "8001", "8002", "8003", "8004", "8005" };
+        string[] stuName = new string[] { "劉學有", "張杰輪", "周立宏", "王吉吉", "陶得華" };
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*
+            if ((e.KeyChar.CompareTo('0')<0 || e.KeyChar.CompareTo('9')>0)&& e.KeyChar!='\b')
+            {
+                e.Handled = true;
+            }
+            */
+            if (!((e.KeyChar.CompareTo('0') >= 0 && e.KeyChar.CompareTo('9') <= 0) || e.KeyChar == '\b'))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = Array.IndexOf(stuId, textBox1.Text);
+            if (index==-1)
+            {
+                label2.Text = "sorry!";
+            }
+            else
+            {
+                label2.Text = "歡迎" + stuName[index];
+            }
+            
+        }
+    }
+}
---
+++ 聽音檔打出英文單字/10910111_2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace _10910111_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SoundPlayer player;
+        int testnum;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Focus();
+            label1.Text = "";
+            Random rd = new Random();
+            testnum = rd.Next(1, 11);
+            player = new SoundPlayer(testnum+".wav");
+            player.Load();
+            player.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.Play();
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] ary = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+            if (textBox1.Text.ToLower() == ary[testnum-1])
+            {
+                label1.Text = "答對了";
+
+            }
+            else
+            {
+                label1.Text = "答錯了";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
